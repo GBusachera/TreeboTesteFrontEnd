203cc645f4d127c9acf6ac02b0721fa31b61c54a
GBusachera <gabrielbusachera1@gmail.com>
2024-07-05 15:27:01 -0300

    T003: feat - Realizado a integração do services na taskList e feito o .map para o taskItem

--- treebo/src/components/taskItem.tsx
+++ treebo/src/components/taskItem.tsx
@@ -1,9 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 
-const TaskItem = () => {
+const TaskItem = (props: any) => {
+
+    const task = props.task
+    const [show, setShow] = useState<boolean>()
+
+    const onclick = () => {
+        if (show == false) {
+            setShow(true)
+        } else {
+            setShow(false)
+        }
+    }
+
     return (
-        <div>
-            <h1>Task Item</h1>
+        <div className="p-6 border-color-black">
+            <h1>{task.name}</h1>
+            <button type="button" onClick={onclick}>Descrição</button>
+            {show && (<p>{task.description}</p>)}
         </div>
     );
 }
--- treebo/src/components/taskList.tsx
+++ treebo/src/components/taskList.tsx
@@ -1,11 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import TaskItem from "./taskItem";
+import { getAllTasks } from "../services/treeboAPI-service";
+
+
 
 const TaskList = () => {
+    useEffect(() => { getTasks() }, []);
+
+    const [tasks, setTasks] = useState([]);
+    const getTasks = async () => {
+        const response = await getAllTasks()
+        const data = await response;
+        setTasks(data);
+    }
+
     return (
         <div>
             <h2>Task List</h2>
-            <TaskItem/>
+            {tasks.map((task, index) => 
+                <TaskItem key={index} task={task} />
+            )}
         </div>
     );
 }
