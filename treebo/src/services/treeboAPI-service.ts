f2b4c2d6350e30990649c7f337d43e0a6e85d450
GBusachera <gabrielbusachera1@gmail.com>
2024-07-05 20:06:52 -0300

    T003: feat - criado a ligação com o backend

--- treebo/src/services/treeboAPI-service.ts
+++ treebo/src/services/treeboAPI-service.ts
@@ -11,4 +11,31 @@ const getAllTasks = async () => {
     }
 }
 
-export { getAllTasks };
+const createTask = async (data: any) => {
+    try {
+        const response = await axios.post(`${API_URL}`, data)
+        return response.data
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+const editTask = async (data: any, id: number) => {
+    try {
+        const response = await axios.patch(`${API_URL}/${id}`, data)
+        return response.data
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+const delTask = async (id: number) => {
+    try {
+        const response = await axios.delete(`${API_URL}/${id}`)
+        return response.data
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+export { getAllTasks, createTask, editTask, delTask };
